57382cb847c14edab5907a3f385df865fa602fd5
Abhicoding <shinganeabhishek@gmail.com>
2018-08-23 20:15:31 +0530

    Added updateDOMProperties function

--- render.js
+++ render.js
@@ -1,19 +1,18 @@
+// let x = {
+//   type: 'div',
+//   props: {
+//     id: '',
+//     children: [{type: 'p', props:{ children:[{type: 'TEXT', props: {
+//       nodeValue: `When you do it your way you can go anywhere you choose. 
+//       Think about a cloud. Just float around and be there. Be so very light. 
+//       Be a gentle whisper.`}}]}}, 
+//     {type: 'TEXT', props: {nodeValue: 'Foo', children:[]}}]
+//   }
+// }
 
-let x = {
-  type: 'div',
-  props: {
-    id: '',
-    children: [{type: 'p', props:{ children:[{type: 'TEXT', props: {
-      nodeValue: `When you do it your way you can go anywhere you choose. 
-      Think about a cloud. Just float around and be there. Be so very light. 
-      Be a gentle whisper. We tell people sometimes: we're like drug dealers, 
-      come into town and get everybody absolutely addicted to painting. 
-      It doesn't take much to get you addicted.`}}]}}, 
-    {type: 'TEXT', props: {nodeValue: 'Foo', children:[]}}]
-  }
-}
+let previousState = null
 
-function render (element, parentDOM) {
+function render (element, parentDOM) { //IN: newVnode, oldVnode, RealDOM OUT:
   const dom = document.createElement(element.type)
   
   // Attaching event listeners
@@ -23,7 +22,7 @@ function render (element, parentDOM) {
     .forEach(name => dom.addEventListener(name.slice(2).toLowerCase(), props[name]))
 
   // Adding the attributes
-  Object.keys(element.props).filter(value => !isListener(value))
+  Object.keys(element.props).filter(value => !isListener(value) && value !== 'children')
     .forEach(name => dom.setAttribute(name, element.props[name]))
 
   // Recursively appending child elements to the parent node
@@ -32,11 +31,48 @@ function render (element, parentDOM) {
       if (child.type === 'TEXT') {    // Adding text nodes
         const text = document.createTextNode(child.props.nodeValue)
         return dom.appendChild(text) 
-      }  
+      }
       render(child, dom)
     })
   }
   return parentDOM.appendChild(dom)
+}
+
+function reco (parentDOM, newVNode, oldVNode) { // IN oldNode, newNode, parent DOM; OUT dom, newVnode
+  let dom
+  newVNode.type === 'TEXT' 
+    ? dom = document.createTextNode(newVNode.props.nodeValue)
+    : dom = document.createElement(newVNode.type)
+  
+  dom = updateDOMProperties(dom, newVNode.props, oldVNode.props,)
+  
+
+}
+
+function updateDOMProperties (dom, newProps, prevProps={}) {
+  const isListener = listener => listener.startsWith('on')
+  const tempObject = Object.assign({}, prevProps, newProps)
+  let keys = Object.keys(tempObject).filter(x => x !== 'children')
+
+  for (let x of keys) {
+    if (x in newProps) {
+      if (newProps[x] === prevProps[x]){
+        continue
+      }
+      if (!isListener(x)) {
+        dom.setAttribute(x, newProps[x])
+        continue
+      }
+      dom.addEventListener(x.slice(2).toLowerCase(), newProps[x])
+      continue
+    }
+    if (!isListener(x)) {
+      dom.removeAttribute(x, prevProps[x])
+      continue
+    }
+    dom.removeEventLister(x.slice(2).toLowerCase(), prevProps[x])
+  }
+  return dom  
 }
 
 module.exports = render
